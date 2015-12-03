b72e21cd24c74252fc9a2e46b1d279def9acc012
slg2011 <catalin_07cat@yahoo.com>
2015-12-03 12:07:55 +0200

    Test commit

--- MyDropBox/MyDropBox/Program.cs
+++ MyDropBox/MyDropBox/Program.cs
@@ -17,6 +17,8 @@ namespace MyDropBox
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+
         }
     }
 }
